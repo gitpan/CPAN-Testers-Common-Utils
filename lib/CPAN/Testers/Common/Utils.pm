7fa8e8276876bb59f20c21bcd5cecbd101daa5b2
David Golden <dagolden@cpan.org>
2010-06-04 14:38:13 +0000

    Import of DAGOLDEN/CPAN-Testers-Common-Utils-0.002 from CPAN.
    gitpan-cpan-distribution: CPAN-Testers-Common-Utils
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         DAGOLDEN/CPAN-Testers-Common-Utils-0.002.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/CPAN/Testers/Common/Utils.pm
+++ lib/CPAN/Testers/Common/Utils.pm
@@ -1,15 +1,19 @@
-# Copyright (c) 2010 by David Golden. All rights reserved.
-# Licensed under Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License was distributed with this file or you may obtain a
-# copy of the License from http://www.apache.org/licenses/LICENSE-2.0
-
-package CPAN::Testers::Common::Utils;
+# 
+# This file is part of CPAN-Testers-Common-Utils
+# 
+# This software is Copyright (c) 2010 by David A Golden.
+# 
+# This is free software, licensed under:
+# 
+#   The Apache License, Version 2.0, January 2004
+# 
 use strict;
 use warnings;
-
-our $VERSION = '0.001';
-$VERSION = eval $VERSION; ## no critic
+package CPAN::Testers::Common::Utils;
+BEGIN {
+  $CPAN::Testers::Common::Utils::VERSION = '0.002';
+}
+# ABSTRACT: Utility functions for CPAN Testers modules
 
 use Exporter ();
 our @ISA = qw/Exporter/;
@@ -33,6 +37,9 @@ our %EXPORT_TAGS = (
 # Lower case is canonical
 my $base_guid = "ed372d00-b19f-3f77-b713-d32bba55d77f";
 
+# strip leading zeros on extraction
+my $nntp_re = qr{\A0*([0-9]{1,7})-b19f-3f77-b713-d32bba55d77f$};
+
 sub nntp_to_guid {
   my ($nntp_id) = @_;
   my $guid = $base_guid;
@@ -42,113 +49,112 @@ sub nntp_to_guid {
 
 sub guid_to_nntp {
   my ($guid) = @_;
-  my ($nntp_id) = $guid =~ m{\A0*([0-9]{1,7})}; # strip leading zeros
+  my ($nntp_id) = $guid =~ $nntp_re;
   return $nntp_id;
 }
 
 1;
 
-__END__
 
-=begin wikidoc
 
-= NAME
+=pod
+
+=head1 NAME
 
 CPAN::Testers::Common::Utils - Utility functions for CPAN Testers modules
 
-= VERSION
+=head1 VERSION
 
-This documentation describes version %%VERSION%%.
+version 0.002
 
-= SYNOPSIS
+=head1 SYNOPSIS
 
-    use CPAN::Testers::Common::Utils ':all';
+     use CPAN::Testers::Common::Utils ':all';
+ 
+     # NNTP ID <=> GUID mapping
+     $guid    = nntp_to_guid( $nntp_id );
+     $nntp_id = guid_to_nntp( $guid    );
 
-    # NNTP ID <=> GUID mapping
-    $guid    = nntp_to_guid( $nntp_id );
-    $nntp_id = guid_to_nntp( $guid    );
-
-= DESCRIPTION
+=head1 DESCRIPTION
 
 This module contains common utility functions for use by other CPAN Testers
 modules
 
-= USAGE
+=head1 USAGE
 
-== Mapping NNTP IDs to GUIDs
+=head2 Mapping NNTP IDs to GUIDs
 
 Legacy CPAN Testers reports were sent via email and made available via an
-NNTP group, C<perl.cpan.testers>.  Reports were 'indexed' by their NNTP ID.
+NNTP group, CE<lt>perl.cpan.testersE<gt>.  Reports were 'indexed' by their NNTP ID.
 The next generation of CPAN Testers uses a GUID URN to identify reports.
 
 Old reports with an NNTP ID are mapped to GUIDs by replacing the first 8
 hex characters of a common 'base GUID' with a zero-padded decimal
 representation of the NNTP ID.
 
-  XXXXXXXX-b19f-3f77-b713-d32bba55d77f
+   XXXXXXXX-b19f-3f77-b713-d32bba55d77f
 
 Such GUID URNs are visually distinctive and have the nice feature of
 sorting earlier than second-generated report GUIDs based on a timestamp.
 
 Two translation functions are provided for convenience.
 
-=== {nntp_to_guid}
+=head3 C<<< nntp_to_guid >>>
 
-    $guid    = nntp_to_guid( $nntp_id );
+     $guid    = nntp_to_guid( $nntp_id );
 
 Given a numeric NNTP ID, returns a standard string-form GUID.  (No range
 checking is done.) Examples:
 
-  nntp_to_guid( 51432   );  # 00051432-b19f-3f77-b713-d32bba55d77f
-  nntp_to_guid( 6171265 );  # 06171265-b19f-3f77-b713-d32bba55d77f
+   nntp_to_guid( 51432   );  # 00051432-b19f-3f77-b713-d32bba55d77f
+   nntp_to_guid( 6171265 );  # 06171265-b19f-3f77-b713-d32bba55d77f
 
-=== {guid_to_nntp}
+=head3 C<<< guid_to_nntp >>>
 
-    $guid    = nntp_to_guid( $nntp_id );
+     $guid    = nntp_to_guid( $nntp_id );
 
 Given a GUID string of the form described above, returns the decimal number
-in the first 8 characaters.  (Again, there is no error checking that
-the GUID is properly formatted.)  Examples:
+in the first 8 characaters.  Examples:
 
-  guid_to_nntp( '00051432-b19f-3f77-b713-d32bba55d77f' ); # 51432
-  guid_to_nntp( '06171265-b19f-3f77-b713-d32bba55d77f' ); # 6171265
+   guid_to_nntp( '00051432-b19f-3f77-b713-d32bba55d77f' ); # 51432
+   guid_to_nntp( '06171265-b19f-3f77-b713-d32bba55d77f' ); # 6171265
 
-= BUGS
+If the GUID string is not derived from the base GUID, this function 
+returns C<<< undef >>>.
+
+=head1 BUGS
 
 Please report any bugs or feature requests using the CPAN Request Tracker
-web interface at [http://rt.cpan.org/Dist/Display.html?Queue=CPAN-Testers-Common-Utils]
+web interface at L<http://rt.cpan.org/Dist/Display.html?Queue=CPAN-Testers-Common-Utils>
 
 When submitting a bug or request, please include a test-file or a patch to an
 existing test-file that illustrates the bug or desired feature.
 
-= SEE ALSO
+=head1 SEE ALSO
 
-* Data::GUID::Any
+=over
 
-= AUTHOR
+=item *
 
-David A. Golden (DAGOLDEN)
+Data::GUID::Any
 
-= COPYRIGHT AND LICENSE
+=back
 
-Copyright (c) 2010 by David A. Golden. All rights reserved.
+=head1 AUTHOR
 
-Licensed under Apache License, Version 2.0 (the "License").
-You may not use this file except in compliance with the License.
-A copy of the License was distributed with this file or you may obtain a
-copy of the License from http://www.apache.org/licenses/LICENSE-2.0
+  David A Golden <dagolden@cpan>
 
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
+=head1 COPYRIGHT AND LICENSE
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+This software is Copyright (c) 2010 by David A Golden.
 
-=end wikidoc
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
 
 =cut
+
+
+__END__
+
 
